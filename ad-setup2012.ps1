d707ed83d5d5f2463f1ea54c20f6d12e5206b19b
dimdung <dimdung@live.com>
2016-06-18 14:00:13 -0400

    Create ad-setup2012.ps1

---
+++ ad-setup2012.ps1
@@ -0,0 +1,16 @@
+#
+# Windows PowerShell script for AD DS Deployment
+#
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "Win2012R2" `
+-DomainName "helixtech.com" `
+-DomainNetbiosName "HELIXTECH" `
+-ForestMode "Win2012R2" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
